f2379524e71bff7dc9f0b07ea38eaac38ca90375
Malasani Yogitha <malasani.yogitha2024@vitstudent.ac.in>
2026-04-08 12:01:59 +0530

    Add files via upload

---
+++ letters.js
@@ -0,0 +1,85 @@
+const letters = [
+
+{
+day:1,
+title:"Day 1",
+content:`[Add your message for Day 1 here]`
+},
+
+{
+day:2,
+title:"Day 2",
+content:`[Add your message for Day 2 here]`
+},
+
+{
+day:3,
+title:"Day 3",
+content:`[Add your message for Day 3 here]`
+},
+
+{
+day:4,
+title:"Day 4",
+content:`[Add your message for Day 4 here]`
+},
+
+{
+day:5,
+title:"Day 5",
+content:`[Add your message for Day 5 here]`
+},
+
+{
+day:6,
+title:"Day 6",
+content:`[Add your message for Day 6 here]`
+},
+
+{
+day:7,
+title:"Day 7",
+content:`[Add your message for Day 7 here]`
+}
+
+];
+
+
+const wordPuzzles = {
+
+1: {
+word: "WORD1",
+hint: "Add a hint for Day 1 puzzle"
+},
+
+2: {
+word: "WORD2",
+hint: "Add a hint for Day 2 puzzle"
+},
+
+3: {
+word: "WORD3",
+hint: "Add a hint for Day 3 puzzle"
+},
+
+4: {
+word: "WORD4",
+hint: "Add a hint for Day 4 puzzle"
+},
+
+5: {
+word: "WORD5",
+hint: "Add a hint for Day 5 puzzle"
+},
+
+6: {
+word: "WORD6",
+hint: "Add a hint for Day 6 puzzle"
+},
+
+7: {
+word: "WORD7",
+hint: "Add a hint for Day 7 puzzle"
+}
+
+};
